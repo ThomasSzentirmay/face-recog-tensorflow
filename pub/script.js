0e03d0124f5b916f11964b40b269241f5823ca07
Thomas Szentirmay <tlszent@outlook.com>
2024-01-15 13:09:51 +1100

    draw detections on canvas

--- pub/script.js
+++ pub/script.js
@@ -6,7 +6,7 @@ Promise.all([
     faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
     faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
     faceapi.nets.faceExpressionNet.loadFromUri("./models")
-])
+]).then(vid)
 
 // cam functionality
 function vid() {
@@ -17,4 +17,16 @@ function vid() {
     )
 }
 
-vid();
+// load detections upon play
+video.addEventListener("play", () => {
+    const canvas = faceapi.createCanvasFromMedia(video);
+    document.body.append(canvas);
+    const displaySize = { width: video.width, height: video.height }
+    setInterval(async () => {
+        const detections = await faceapi.detectAllFaces(video,
+            new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
+            .withFaceExpressions();
+        const resizedDetections = faceapi.resizeResults(detections, displaySize);
+        faceapi.draw.drawDetections(canvas, resizedDetections);
+    }, 100)
+})
