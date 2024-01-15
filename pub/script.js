10a7f325c6b491615186e25763cb87f766c536b6
Thomas Szentirmay <tlszent@outlook.com>
2024-01-15 13:00:50 +1100

    downloaded models and integrated in script

--- pub/script.js
+++ pub/script.js
@@ -1,5 +1,14 @@
 const video = document.getElementById("video");
 
+// load models
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+    faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+    faceapi.nets.faceExpressionNet.loadFromUri("./models")
+])
+
+// cam functionality
 function vid() {
     navigator.getUserMedia(
         { video: {} },
